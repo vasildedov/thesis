1122a01be79d235d6d660cd1bc55ec6de5f5cd86
dedov <dedovvasil@gmail.com>
2024-10-27 16:40:34 +0200

    add xgb

--- m4.py
+++ m4.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.preprocessing import StandardScaler
 from datasetsforecast.m4 import M4, M4Info, M4Evaluation
 from utils.preprocess_M4 import train_test_split, create_train_windows, create_test_windows, recursive_predict
-from utils.ml_models import LGBMModel, calculate_smape
+from utils.ml_models import LGBMModel, calculate_smape, XGBModel
 
 train, test = train_test_split('Hourly')
 
@@ -18,12 +18,15 @@ X_train, y_train, train_ids = create_train_windows(train, look_back, horizon)
 lgbm_model = LGBMModel()
 lgbm_model.fit(X_train, y_train)
 
+xgb_model = XGBModel()
+xgb_model.fit(X_train, y_train)
+
 # Prepare the test window (last 60 hours of each series from the training set)
 X_test = create_test_windows(train, look_back)
 
 # Predict on the test set
 y_pred = recursive_predict(lgbm_model, X_test, horizon)
-
+y_pred_xgb = recursive_predict(xgb_model, X_test, horizon)
 # Function to calculate sMAPE
 
 # Calculate sMAPE on the test dataset
@@ -33,4 +36,8 @@ smape = calculate_smape(y_true, y_pred)
 print(f"sMAPE: {smape:.4f}")
 
 # equivalent
+print('LGBM evaluation:')
 M4Evaluation.evaluate('data', 'Hourly', y_pred)
+
+print('XGB evaluation:')
+M4Evaluation.evaluate('data', 'Hourly', y_pred_xgb)
--- utils/ml_models.py
+++ utils/ml_models.py
@@ -1,5 +1,6 @@
 import lightgbm as lgb
 import numpy as np
+import xgboost as xgb
 
 
 def calculate_smape(y_true, y_pred, epsilon=1e-10):
@@ -10,6 +11,19 @@ def calculate_smape(y_true, y_pred, epsilon=1e-10):
 class LGBMModel:
     def __init__(self):
         self.model = lgb.LGBMRegressor()
+
+    def fit(self, X, y):
+        y_flat = y[:, 0]  # Predict the first value of the horizon
+        self.model.fit(X, y_flat)
+
+    def predict(self, X):
+        return self.model.predict(X).reshape(-1, 1)
+
+
+# XGBoost model setup
+class XGBModel:
+    def __init__(self):
+        self.model = xgb.XGBRegressor(objective='reg:squarederror')
 
     def fit(self, X, y):
         y_flat = y[:, 0]  # Predict the first value of the horizon
