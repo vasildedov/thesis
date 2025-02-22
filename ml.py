ff5b75f313b325fdf3c36116e939d5e2d9695656
dedov <dedovvasil@gmail.com>
2025-02-22 16:23:49 +0200

    add for loops for all models in ml.py, stats.py

--- ml.py
+++ ml.py
@@ -2,49 +2,47 @@ from data_preprocessing.preprocess_ml import create_train_windows, create_test_w
 from models.train_ml import train_and_save_model
 from models.models_ml import LGBMModel, XGBModel
 
-
-# ===== Dataset =====
-dataset = 'etth2'
-freq = 'default'
-multivariate = False
-
-if dataset == 'm3':
-    from data_preprocessing.preprocess_m3 import train_test_split
-    freq = freq.capitalize()
-elif dataset == 'm4':
-    from data_preprocessing.preprocess_m4 import train_test_split
-    freq = freq.capitalize()
-elif dataset == 'tourism':
-    from data_preprocessing.preprocess_tourism import train_test_split
-    freq = freq.lower()
-else:
-    from data_preprocessing.preprocess_ett import train_test_split, get_windows
-    multivariate = True
-
 # ===== Parameters =====
-retrain = False
-direct = True
+retrain_mode = False
 
-# Load train and test data
-if not multivariate:
-    train, test, horizon = train_test_split(freq)
-    look_back = 2*horizon if not (dataset == 'tourism' and freq == 'yearly') else 7
-    # Generate windows for training
-    X_train, y_train = create_train_windows(train, look_back, horizon)
-    # Prepare the test window (last 'look_back' points of each series)
-    X_test = create_test_windows(train, look_back)
-    y_test = test['y'].values.reshape(test['unique_id'].nunique(), horizon)
-else:
-    train, val, test = train_test_split(group=dataset)
-    look_back = 720
-    horizon = 24
-    X_train, y_train, X_val, y_val, X_test, y_test = get_windows(train, val, test, look_back, horizon)
-
-# ===== Models =====
-# LightGBM
-lgbm_model = LGBMModel(direct=direct)
-y_pred_lgbm, eval_lgbm = train_and_save_model(lgbm_model, "LGBM", X_train.reshape(X_train.shape[0], -1), y_train, X_test.reshape(X_test.shape[0], -1), y_test, horizon, freq, look_back, retrain, dataset, direct)
-
-# XGBoost
-xgb_model = XGBModel(direct=direct)
-y_pred_xgb, eval_xgb = train_and_save_model(xgb_model, "XGB", X_train.reshape(X_train.shape[0], -1), y_train, X_test.reshape(X_test.shape[0], -1), y_test, horizon, freq, look_back, retrain, dataset, direct)
+for direct in [True, False]:
+    for dataset in ['tourism', 'm3', 'm4', 'etth1', 'etth2']:
+        multivariate = False
+        if dataset == 'm3':
+            from data_preprocessing.preprocess_m3 import train_test_split
+            freqs = ['Other', 'Monthly', 'Quarterly', 'Yearly']
+        elif dataset == 'm4':
+            from data_preprocessing.preprocess_m4 import train_test_split
+            freqs = ['Hourly', 'Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']
+        elif dataset == 'tourism':
+            from data_preprocessing.preprocess_tourism import train_test_split
+            freqs = ['monthly', 'quarterly', 'yearly']
+        else:
+            from data_preprocessing.preprocess_ett import train_test_split, get_windows
+            freqs = ['default']
+            multivariate = True
+        
+        for freq in freqs:                
+            # Load train and test data
+            if not multivariate:
+                train, test, horizon = train_test_split(freq)
+                look_back = 2*horizon if not (dataset == 'tourism' and freq == 'yearly') else 7
+                # Generate windows for training
+                X_train, y_train = create_train_windows(train, look_back, horizon)
+                # Prepare the test window (last 'look_back' points of each series)
+                X_test = create_test_windows(train, look_back)
+                y_test = test['y'].values.reshape(test['unique_id'].nunique(), horizon)
+            else:
+                train, val, test = train_test_split(group=dataset)
+                look_back = 720
+                horizon = 24
+                X_train, y_train, X_val, y_val, X_test, y_test = get_windows(train, val, test, look_back, horizon)
+            
+            # ===== Models =====
+            # LightGBM
+            lgbm_model = LGBMModel(direct=direct)
+            y_pred_lgbm, eval_lgbm = train_and_save_model(lgbm_model, "LGBM", X_train.reshape(X_train.shape[0], -1), y_train, X_test.reshape(X_test.shape[0], -1), y_test, horizon, freq, look_back, retrain_mode, dataset, direct)
+            
+            # XGBoost
+            xgb_model = XGBModel(direct=direct)
+            y_pred_xgb, eval_xgb = train_and_save_model(xgb_model, "XGB", X_train.reshape(X_train.shape[0], -1), y_train, X_test.reshape(X_test.shape[0], -1), y_test, horizon, freq, look_back, retrain_mode, dataset, direct)
--- stats.py
+++ stats.py
@@ -7,93 +7,102 @@ from datetime import datetime
 from data_preprocessing.params_stats import get_params
 from utils.helpers import evaluate
 
-dataset = 'etth2'
-freq = 'hourly'.capitalize()  # Options: 'Yearly', 'Quarterly', 'Monthly', 'Weekly', 'Daily', 'Hourly'
-model_type = 'SARIMA'
-multivariate = False
+for model_type in ['ARIMA', 'SARIMA']:
+    for dataset in ['tourism', 'm3', 'm4', 'etth1', 'etth2']:
+        multivariate = False
+        if dataset == 'm3':
+            from data_preprocessing.preprocess_m3 import train_test_split
+            freqs = ['Other', 'Monthly', 'Quarterly', 'Yearly']
+        elif dataset == 'm4':
+            from data_preprocessing.preprocess_m4 import train_test_split
+            freqs = ['Hourly', 'Daily', 'Weekly', 'Monthly', 'Quarterly', 'Yearly']
+        elif dataset == 'tourism':
+            from data_preprocessing.preprocess_tourism import train_test_split
+            freqs = ['monthly', 'quarterly', 'yearly']
+        else:
+            from data_preprocessing.preprocess_ett import train_test_split, get_windows
+            freqs = ['default']
+            multivariate = True
 
-order, seasonal_order, asfreq = get_params(freq, model_type)
 
-if dataset == 'm3':
-    from data_preprocessing.preprocess_m3 import train_test_split
-elif dataset == 'm4':
-    from data_preprocessing.preprocess_m4 import train_test_split
-elif dataset == 'tourism':
-    from data_preprocessing.preprocess_tourism import train_test_split
-    freq = freq.lower()
-else:
-    from data_preprocessing.preprocess_ett import train_test_split, get_windows
-    multivariate = True
+        for freq in freqs:
 
-# Load data
-if not multivariate:
-    train, test, horizon = train_test_split(freq)
-    if dataset != 'm4':
-        train.set_index('ds', inplace=True)
-else:
-    train, val, test = train_test_split(group=dataset, multivariate=True)
-    horizon = 24
-    X_train, y_train, X_val, y_val, X_test, y_test = get_windows(train, val, test, 720, horizon, get_exog=True)
-    train['unique_id'] = dataset
+            # don't train unnecessary yearly model for SARIMA - assume no seasonality for yearly data
+            if (model_type == 'SARIMA' and freq.lower() == 'yearly'):
+                continue
 
-# Define the folder to save all models
-model_folder = f"models/{dataset}/recursive/stats_{freq.lower()}/"
-os.makedirs(model_folder, exist_ok=True)
+            order, seasonal_order, asfreq = get_params(freq, model_type)
 
-start_overall_time = time.time()
-# Using parallel processing to speed up training and forecasting
-if not multivariate:
-    forecasts = [
-        train_and_forecast(
-            train[train['unique_id'] == uid]['y'].asfreq(asfreq) if (asfreq and dataset != 'm4') else
-            train[train['unique_id'] == uid]['y'],
-            unique_id=uid,
-            model_type=model_type,
-            order=order,
-            seasonal_order=seasonal_order,
-            horizon=horizon,
-            model_folder=model_folder
-        )
-        for uid in train['unique_id'].unique()
-    ]
-else:
-    forecasts = [
-        train_and_forecast(
-            X_test[i, :, -1],
-            unique_id=f'{i}',
-            model_type=model_type,
-            order=order,
-            seasonal_order=seasonal_order,
-            horizon=horizon,
-            model_folder=model_folder,
-            exogenous_train=X_test[i, :, :-1],
-            exogenous_test=y_test[i, :, :-1]
-        )
-        for i in range(X_test.shape[0])
-    ]
-end_overall_time = time.time()
+            # Load data
+            if not multivariate:
+                train, test, horizon = train_test_split(freq)
+                if dataset != 'm4':
+                    train.set_index('ds', inplace=True)
+            else:
+                train, val, test = train_test_split(group=dataset, multivariate=True)
+                horizon = 24
+                X_train, y_train, X_val, y_val, X_test, y_test = get_windows(train, val, test, 720, horizon,
+                                                                             get_exog=True)
+                train['unique_id'] = dataset
 
-# Convert forecasts to numpy array
-y_pred = np.array(forecasts)
+            # Define the folder to save all models
+            model_folder = f"models/{dataset}/recursive/stats_{freq.lower()}/"
+            os.makedirs(model_folder, exist_ok=True)
 
-# Reshape true values
-y_true = test['y'].values.reshape(-1, horizon) if not multivariate else y_test[:, :, -1].copy()
+            start_overall_time = time.time()
+            # Using parallel processing to speed up training and forecasting
+            if not multivariate:
+                forecasts = [
+                    train_and_forecast(
+                        train[train['unique_id'] == uid]['y'].asfreq(asfreq) if (asfreq and dataset != 'm4') else
+                        train[train['unique_id'] == uid]['y'],
+                        unique_id=uid,
+                        model_type=model_type,
+                        order=order,
+                        seasonal_order=seasonal_order,
+                        horizon=horizon,
+                        model_folder=model_folder
+                    )
+                    for uid in train['unique_id'].unique()
+                ]
+            else:
+                forecasts = [
+                    train_and_forecast(
+                        X_test[i, :, -1],
+                        unique_id=f'{i}',
+                        model_type=model_type,
+                        order=order,
+                        seasonal_order=seasonal_order,
+                        horizon=horizon,
+                        model_folder=model_folder,
+                        exogenous_train=X_test[i, :, :-1],
+                        exogenous_test=y_test[i, :, :-1]
+                    )
+                    for i in range(X_test.shape[0])
+                ]
+            end_overall_time = time.time()
 
-# Evaluate forecasts
-evaluation = evaluate(y_true, y_pred)
+            # Convert forecasts to numpy array
+            y_pred = np.array(forecasts)
 
-# Save evaluation metadata
-metadata_path = os.path.join(model_folder, f"{model_type.lower()}_metadata.json")
-metadata = {
-    "model_name": model_type,
-    "frequency": freq.lower(),
-    "order": order,
-    "seasonal_order": seasonal_order,
-    "horizon": horizon,
-    **evaluation,
-    "time_to_train": round(end_overall_time-start_overall_time, 2),
-    "timestamp": datetime.now().isoformat()
-}
-with open(metadata_path, "w") as f:
-    json.dump(metadata, f, indent=4)
-print(f"Metadata saved to {metadata_path}")
+            # Reshape true values
+            y_true = test['y'].values.reshape(-1, horizon) if not multivariate else y_test[:, :, -1].copy()
+
+            # Evaluate forecasts
+            evaluation = evaluate(y_true, y_pred)
+
+            # Save evaluation metadata
+            metadata_path = os.path.join(model_folder, f"{model_type.lower()}_metadata.json")
+            metadata = {
+                "model_name": model_type,
+                "frequency": freq.lower(),
+                "order": order,
+                "seasonal_order": seasonal_order,
+                "horizon": horizon,
+                **evaluation,
+                "time_to_train": round(end_overall_time-start_overall_time, 2),
+                "timestamp": datetime.now().isoformat()
+            }
+            with open(metadata_path, "w") as f:
+                json.dump(metadata, f, indent=4)
+            print(f"Metadata saved to {metadata_path}")
